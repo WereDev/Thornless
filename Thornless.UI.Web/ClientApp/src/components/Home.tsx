e7e23836f7c6655be241ecc526d84fa9b2a088d6
WereDev <github@weredev.com>
2020-07-23 19:33:02 -0500

    adding the beginnings of styling and limiting bootstrap

--- Thornless.UI.Web/ClientApp/src/components/Home.tsx
+++ Thornless.UI.Web/ClientApp/src/components/Home.tsx
@@ -2,19 +2,31 @@ import * as React from 'react';
 import { NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { CharactersSvg } from '../shared';
+
+import './Home.scss';
 
 const Home = () => (
 
   <div>
-    <h1>Thornless</h1>
     <p>
       A project that I'm slowly (very slowly) working on to help with
       generating character names for Pathfinder, Dungeons &amp; Dragons,
       and other role-playing or fantasy settings.
     </p>
-    <p>
-      <NavLink tag={Link} className="text-dark" to="/characternames">Character Names</NavLink>
-    </p>
+    <div className="row">
+      <div className="col-md-4 col-12 mt-4 text-center">
+        <NavLink tag={Link} to="/characternames">
+          <CharactersSvg />
+        </NavLink>
+      </div>
+      <div className="col-md-4 col-12 mt-4">
+        {/* <BuildingsSvg /> */}
+      </div>
+      <div className="col-md-4 col-12 mt-4">
+        {/* <Settlements /> */}
+      </div>
+    </div>
   </div>
 );
 
---
+++ Thornless.UI.Web/ClientApp/src/shared/index.tsx
@@ -0,0 +1,15 @@
+export {default as BuildingsSvg} from './buildings.svg';
+export {default as CharactersSvg} from './characters.svg';
+export {default as Home} from './home.svg';
+export {default as Settlements} from './settlements.svg';
+export {default as ThornlessSvg} from './thornless.svg';
+
+export interface ApiResponse<T> {
+    data: T;
+}
+
+export enum LoadingStates {
+    IsLoading,
+    IsLoaded,
+    IsNotStarted
+};
